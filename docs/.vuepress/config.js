01b1f24bd9103dcb4dfda7fec7fe713d85eb29e5
刘一先 <1012434956@qq.com>
2019-05-13 17:39:06 +0800

    初次

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: "嘟嘟桌面使用文档",
+  description: "嘟嘟桌面使用文档",
+  themeConfig: {
+    search: false,
+    nav: [
+      {
+        text: "使用指南",
+        link: "/"
+      },
+      {
+        text: "github项目地址",
+        link: "https://github.com/Soap613/dudu-launcher/"
+      },
+      {
+        text: "码云项目地址",
+        link: "https://gitee.com/helloworld_studio/dudu-launcher/"
+      }
+    ],
+    sidebar: {
+      "/": ["/", "theme", "item", "ble", "driving", "diceng", "other"]
+    },
+    sidebarDepth: 2
+  }
+};
